bfab1fb8463478212f3f2f3e20663fe969e5aa12
jiangshucong <jiangshucong@do1.com.cn>
2019-12-21 10:10:08 +0800

    【新增后端接口，database】

--- react-blog/service/app/controller/default/home.js
+++ react-blog/service/app/controller/default/home.js
@@ -1,13 +1,30 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
-    let result = await this.app.mysql.get('blog_content', {})
-    console.log(result);
-    
-    this.ctx.body = result;
+    let result = await this.app.mysql.get("blog_content", {});
+
+    // this.ctx.body = result;
+    this.ctx.body = "hi.jsc";
+  }
+
+  async getArticleList() {
+    let sql =
+      "SELECT article.id as id," +
+      "article.title as title," +
+      "article.introduce as introduce," +
+      "FROM_UNIXTIME(article.addTime, '%Y-%m-%d %H:%i:%s') as addTime," +
+      "article.view_count as view_count ," +
+      ".type.typeName as typeName " +
+      "FROM article LEFT JOIN type ON article.type_id = type.Id";
+
+    const results = await this.app.mysql.query(sql);
+
+    this.ctx.body = {
+      data: results
+    };
   }
 }
 
